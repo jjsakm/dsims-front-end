ee9ace2aab199e478d4b9e7c827409e90dcb78ea
sallysprism <33612159+sallysprism@users.noreply.github.com>
2025-12-23 08:57:21 +0900

    style : infotable

---
+++ src/components/Table/InfoTable.tsx
@@ -0,0 +1,133 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Stack,
+  TextField,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  Checkbox,
+  FormGroup,
+} from "@mui/material";
+
+export interface CellGroup {
+  label: string;
+  content: string | React.ReactNode | string[];
+  contentColSpan?: number;
+  inputType?: "text" | "textarea" | "radio" | "checkbox" | "custom";
+}
+
+export interface InfoTableRow {
+  groups: CellGroup[];
+}
+
+export interface InfoTableProps {
+  rows: InfoTableRow[];
+}
+
+export const InfoTableView: React.FC<InfoTableProps> = ({ rows }) => {
+  return (
+    <Table>
+      <TableBody>
+        {rows.map((row, ridx) => (
+          <TableRow key={ridx}>
+            {row.groups.map((group, gidx) => (
+              <React.Fragment key={`${ridx}-${gidx}`}>
+                <TableCell>{group.label}</TableCell>
+                <TableCell colSpan={group.contentColSpan ?? 3}>
+                  <Stack direction="row" spacing={1} alignItems="center">
+                    {typeof group.content === "string"
+                      ? group.content
+                      : Array.isArray(group.content)
+                      ? group.content.join(", ")
+                      : group.content}
+                  </Stack>
+                </TableCell>
+              </React.Fragment>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export const InfoTableCreate: React.FC<InfoTableProps> = ({ rows }) => {
+  return (
+    <Table>
+      <TableBody>
+        {rows.map((row, ridx) => (
+          <TableRow key={ridx}>
+            {row.groups.map((group, gidx) => (
+              <React.Fragment key={`${ridx}-${gidx}`}>
+                <TableCell>{group.label}</TableCell>
+                <TableCell colSpan={group.contentColSpan ?? 3}>
+                  <Stack direction="row" spacing={1} alignItems="center">
+                    {(() => {
+                      if (typeof group.content === "string") {
+                        if (group.inputType === "textarea") {
+                          return (
+                            <TextField
+                              defaultValue={group.content}
+                              size="small"
+                              fullWidth
+                              multiline
+                              minRows={3}
+                            />
+                          );
+                        }
+                        return (
+                          <TextField
+                            defaultValue={group.content}
+                            size="small"
+                            fullWidth
+                          />
+                        );
+                      }
+                      if (
+                        Array.isArray(group.content) &&
+                        (group.inputType === "radio" ||
+                          group.inputType === "checkbox")
+                      ) {
+                        if (group.inputType === "radio") {
+                          return (
+                            <RadioGroup row>
+                              {group.content.map((option, i) => (
+                                <FormControlLabel
+                                  key={i}
+                                  value={option}
+                                  control={<Radio />}
+                                  label={option}
+                                />
+                              ))}
+                            </RadioGroup>
+                          );
+                        } else if (group.inputType === "checkbox") {
+                          return (
+                            <FormGroup row>
+                              {group.content.map((option, i) => (
+                                <FormControlLabel
+                                  key={i}
+                                  control={<Checkbox />}
+                                  label={option}
+                                />
+                              ))}
+                            </FormGroup>
+                          );
+                        }
+                      }
+                      return group.content;
+                    })()}
+                  </Stack>
+                </TableCell>
+              </React.Fragment>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
